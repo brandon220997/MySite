fc8bf876d08660b937778337860efbf084cea199
brandon220997 <brandon.cramer1997@gmail.com>
2023-07-19 19:33:51 +1000

    Actually add Window Resize

--- main.js
+++ main.js
@@ -183,4 +183,14 @@ function moveCamera() {
 }
 
 document.body.onscroll = moveCamera;
-window.onresize = moveCamera;
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
